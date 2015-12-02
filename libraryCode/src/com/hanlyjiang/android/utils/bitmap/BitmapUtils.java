bd23c6be8879615b3ca43d08e1bd5df1b4412cbf
hanlyjiang <hanlyjiang@outlook.com>
2015-12-02 09:46:31 +0800

    add src dir

---
+++ libraryCode/src/com/hanlyjiang/android/utils/bitmap/BitmapUtils.java
@@ -0,0 +1,29 @@
+package com.hanlyjiang.android.utils.bitmap;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+
+public class BitmapUtils {
+	
+	/**
+	 * 通过Uri 获取 bitmap 对象
+	 * @param context
+	 * @param uri
+	 * @return
+	 * @throws IOException
+	 */
+	public static Bitmap getBitmapFromUri(Context context,Uri uri) throws IOException {
+	    ParcelFileDescriptor parcelFileDescriptor =
+	    		context.getContentResolver().openFileDescriptor(uri, "r");
+	    FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+	    Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+	    parcelFileDescriptor.close();
+	    return image;
+	}
+}
