28e80f067297cc577d7f79523a33d2a3b52c4446
hanlyjiang <hanlyjiang@outlook.com>
2017-03-16 11:58:36 +0800

    完善行为

--- app/src/main/java/com/geostar/solrtest/ftp/FTPDownloadActivity.java
+++ app/src/main/java/com/geostar/solrtest/ftp/FTPDownloadActivity.java
@@ -39,8 +39,6 @@ public class FTPDownloadActivity extends AppCompatActivity implements View.OnCli
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ftpdownload);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        getSupportActionBar().setHomeButtonEnabled(true);
 
         findViewById(R.id.btn_connect_ftp).setOnClickListener(this);
         findViewById(R.id.btn_login_ftp).setOnClickListener(this);
